c3edafd5743b4309bdd4e9a01c3dc9e7676cea1d
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-08-17 10:10:21 -0500

    Update E1.cpp

--- Sesion01/Ejercicios/E1.cpp
+++ Sesion01/Ejercicios/E1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+
+int sumarElementos(int arreglo[], int tamano) {
+    int suma = 0;
+    for (int i = 0; i < tamano; ++i) {
+        suma += arreglo[i];
+    }
+    return suma;
+}
+
+
+int main() {
+    int arreglo[] = {1, 2, 3, 4, 5};
+    int tamano = sizeof(arreglo) / sizeof(arreglo[0]);
+    int resultado = sumarElementos(arreglo, tamano);
+    cout << "La suma de los elementos es: " << resultado << endl;
+    return 0;
+}
