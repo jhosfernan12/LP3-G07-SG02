654785103743e0681e5165490f7cc2e8c5d1ce38
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-08-31 09:38:21 -0500

    Update A1.java

--- Sesion02/Actividades/A1.java
+++ Sesion02/Actividades/A1.java
@@ -1,1 +1,30 @@
+public class Coche 
+{
+ // Atributos Metodos
 
+
+ // Constructores
+   public Coche()
+   {
+  // Aquí su código
+   }
+   public Coche(String marca, String modelo, int añoFabricacion, double precio)
+   {
+  // Aquí su código
+   }
+   // Métodos
+   public boolean aplicarDescuento(double descuento)
+   {
+   // Aquí su código
+   }
+
+
+public class EjemploCoche 
+{ 
+	public static void main(String[] args) 
+{ 
+// Crear objetos coche 
+// Encender los coches 
+// Acelerar y frenar los coches 
+// Apagar los coches 
+}
