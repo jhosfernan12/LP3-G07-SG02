5541eb884acdcd48ed7bed60268a33e5bc6f7bc8
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-09-10 17:36:10 -0500

    Create E2.java

---
+++ Sesion03/Ejercicios/E2.java
@@ -0,0 +1,54 @@
+package Sesion3;
+
+public abstract class Forma 
+{
+	public void Dibujar()
+	{
+		System.out.println("Dibujando la forma");
+	}
+}
+
+public class Circulo extends Forma
+{
+	@Override 
+	public void Dibujar()
+	{
+		System.out.println("Dibujando un Circulo");
+	}
+
+}
+
+public class Triangulo extends Forma
+}
+	@Override
+	public void Dibujar()
+	{
+		System.out.println("Dibujando un Triangulo");
+	}
+	
+}
+
+public class Cuadrado extends Forma
+{
+	@Override
+	public void Dibujar()
+	{
+		System.out.println("Dibujando un Cuadrado");
+	}
+}
+
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Forma circulo = new Circulo();
+		Forma triangulo = new Triangulo();
+		Forma cuadrado = new Cuadrado();
+		
+		circulo.Dibujar();
+		triangulo.Dibujar();
+		cuadrado.Dibujar();
+		
+    }
+}
