29568d12f29899c0e5bf6d6e743b3e2a4dbe7eb1
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-09-12 19:30:59 -0500

    Update E1.java

--- Sesion03/Ejercicios/E1.java
+++ Sesion03/Ejercicios/E1.java
@@ -1,1 +1,57 @@
+package s3_1;
+
+public abstract class Vehiculo
+  {
+  	protected int velocidad;
+    public Vehiculo() 
+    {
+         this.velocidad = 0;
+   }
+   public abstract void acelerar();
+   public int getVelocidad() 
+   {
+       return velocidad;
+   }
+}
+
+
+
+
+public class Carro extends Vehiculo
+{
+	 @Override
+	 public void acelerar() 
+  {
+		 System.out.println("Acelerando el coche usando el motor.");
+	  velocidad += 10; 
+	 }
+}
+
+
+
+public class Bicicleta extends Vehiculo 
+{
+	 @Override
+	 public void acelerar() 
+  {
+		 System.out.println("Acelerando la bicicleta pedaleando.");
+	 }
+}
+
+
+
+public class Main 
+{
+	public static void main(String[] args) 
+  {
+       Vehiculo carro = new Carro();
+       Vehiculo bicicleta = new Bicicleta();
+
+       carro.acelerar();
+       System.out.println("Velocidad del coche: " + carro.getVelocidad());
+
+       bicicleta.acelerar();
+       System.out.println("Velocidad de la bicicleta: " + bicicleta.getVelocidad());
+   }
+}
 
