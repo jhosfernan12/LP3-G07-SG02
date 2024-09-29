7add40c4c6f9f0d844669ddf75d03ea20ad92cc4
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-09-29 15:42:32 -0500

    Update A1.java

--- Sesion05/Actividades/A1.java
+++ Sesion05/Actividades/A1.java
@@ -1,1 +1,36 @@
+public class PruebaMetodoGenerico 
+{
+
+
+    public static <E> void imprimirArreglo(E[] arregloEntrada) 
+   {
+        for (E elemento : arregloEntrada) 
+        {
+            System.out.printf("%s ", elemento);
+        }
+        System.out.println();
+    } 
+
+
+    public static void main(String args[]) 
+  {
+        Integer[] arregloInteger = {1, 2, 3, 4, 5, 6};
+        Double[] arregloDouble = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
+        Character[] arregloCharacter = {'H', 'O', 'L', 'A'};
+
+
+        System.out.println("El arreglo arregloInteger contiene:");
+        imprimirArreglo(arregloInteger); 
+
+
+        System.out.println("\nEl arreglo arregloDouble contiene:");
+        imprimirArreglo(arregloDouble); 
+
+
+        System.out.println("\nEl arreglo arregloCharacter contiene:");
+        imprimirArreglo(arregloCharacter);
+    } 
+} 
+
+
 
