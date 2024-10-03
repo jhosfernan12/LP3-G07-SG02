9906ff59d6bdd55761189d09610dbb7aab44c303
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-10-02 19:42:25 -0500

    Update E1.java

--- Sesion05/Ejercicios/E1.java
+++ Sesion05/Ejercicios/E1.java
@@ -1,1 +1,54 @@
+public class Par<F, S> 
+{
+    private F primero;
+    private S segundo;
 
+    // Constructor
+    public Par(F primero, S segundo) 
+    {
+        this.primero = primero;
+        this.segundo = segundo;
+    }
+
+   
+    public F getPrimero() 
+    {
+        return primero;
+    }
+
+    public S getSegundo() 
+    {
+        return segundo;
+    }
+
+    // Métodos set
+    public void setPrimero(F primero) 
+    {
+        this.primero = primero;
+    }
+
+    public void setSegundo(S segundo) 
+    {
+        this.segundo = segundo;
+    }
+
+
+    @Override
+    public String toString() 
+    {
+        return "(Primero: " + primero + ", Segundo: " + segundo + ")";
+    }
+
+    public static void main(String[] args) 
+    {
+        Par<Integer, String> par1 = new Par<>(1, "Uno");
+        System.out.println(par1); // Imprimira: (Primero: 1, Segundo: Uno)
+
+        Par<String, Double> par2 = new Par<>("Dos", 2.0);
+        System.out.println(par2); // Imprimira: (Primero: Dos, Segundo: 2.0)
+
+        par1.setPrimero(2);
+        par1.setSegundo("Dos");
+        System.out.println(par1); // Imprimira: (Primero: 2, Segundo: Dos)
+    }
+}
