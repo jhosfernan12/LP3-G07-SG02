d5ba107b2179d6796f0df93a1e9b8c1eb2388f2a
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-10-04 09:14:17 -0500

    Update E3.java

--- Sesion05/Ejercicios/E3.java
+++ Sesion05/Ejercicios/E3.java
@@ -1,17 +1,79 @@
+public class Par<F, S> 
+{
+    private F primero;
+    private S segundo;
 
-public class Main {
-   
-    public static <F, S> void imprimirPar(Par<F, S> par) {
-        System.out.println(par.toString());
+    // Constructor
+    public Par(F primero, S segundo) 
+    {
+        this.primero = primero;
+        this.segundo = segundo;
     }
 
-    public static void main(String[] args) {
-        Par<String, Integer> par1 = new Par<>("Texto", 123);
-        Par<Double, Boolean> par2 = new Par<>(99.99, true);
+    public F getPrimero() 
+    {
+        return primero;
+    }
+
+    public S getSegundo() 
+    {
+        return segundo;
+    }
+
+    public void setPrimero(F primero) 
+    {
+        this.primero = primero;
+    }
+
+    public void setSegundo(S segundo) 
+    {
+        this.segundo = segundo;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "(Primero: " + primero + ", Segundo: " + segundo + ")";
+    }
+}
+
+
+class Persona 
+{
+    private String nombre;
+
+    public Persona(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return nombre;
+    }
+}
+
+
+public class Main 
+{
+  
+    public static <F, S> void imprimirPar(Par<F, S> par) 
+    {
+        System.out.println(par);
+    }
+
+    public static void main(String[] args) 
+    {
+        Par<String, Integer> par1 = new Par<>("Uno", 1);
+        imprimirPar(par1); // Imprime: (Primero: Uno, Segundo: 1)
+
+  
+        Par<Double, Boolean> par2 = new Par<>(3.14, true);
+        imprimirPar(par2); // Imprime: (Primero: 3.14, Segundo: true)
+
+
         Par<Persona, Integer> par3 = new Par<>(new Persona("Juan"), 25);
-      
-        imprimirPar(par1);  // Imprime: (Primero: Texto, Segundo: 123)
-        imprimirPar(par2);  // Imprime: (Primero: 99.99, Segundo: true)
-        imprimirPar(par3);  // Imprime: (Primero: Persona: Juan, Segundo: 25)
+        imprimirPar(par3); // Imprime: (Primero: Juan, Segundo: 25)
     }
 }
