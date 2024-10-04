48cf868cc28ea9e5a21f44b90f2f525293a9062f
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-10-03 19:42:07 -0500

    Create E3.java

---
+++ Sesion05/Ejercicios/E3.java
@@ -0,0 +1,27 @@
+class Persona {
+    private String nombre;
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona: " + nombre;
+    }
+}
+public class Main {
+   
+    public static <F, S> void imprimirPar(Par<F, S> par) {
+        System.out.println(par.toString());
+    }
+
+    public static void main(String[] args) {
+        Par<String, Integer> par1 = new Par<>("Texto", 123);
+        Par<Double, Boolean> par2 = new Par<>(99.99, true);
+        Par<Persona, Integer> par3 = new Par<>(new Persona("Juan"), 25);
+      
+        imprimirPar(par1);  // Imprime: (Primero: Texto, Segundo: 123)
+        imprimirPar(par2);  // Imprime: (Primero: 99.99, Segundo: true)
+        imprimirPar(par3);  // Imprime: (Primero: Persona: Juan, Segundo: 25)
+    }
+}
