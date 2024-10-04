fe05835ea4154a0cd8609c97388398b47922b02d
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-10-03 19:41:24 -0500

    Create E4.java

---
+++ Sesion05/Ejercicios/E4.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+class Par<F, S> {
+    private F primero;
+    private S segundo;
+
+
+    public Par(F primero, S segundo) 
+    {
+        this.primero = primero;
+        this.segundo = segundo;
+    }
+
+
+    @Override
+    public String toString() 
+    {
+        return "(Primero: " + primero + ", Segundo: " + segundo + ")";
+    }
+}
+
+
+class Contenedor<F, S> 
+{
+    private List<Par<F, S>> pares;
+
+
+    public Contenedor() {
+        this.pares = new ArrayList<>();
+    }
+
+
+    public void agregarPar(F primero, S segundo) 
+    {
+        Par<F, S> par = new Par<>(primero, segundo);
+        pares.add(par);
+    }
+
+
+    public Par<F, S> obtenerPar(int indice) 
+    {
+        if (indice < 0 || indice >= pares.size()) 
+        {
+            throw new IndexOutOfBoundsException("Índice fuera de los límites");
+        }
+        return pares.get(indice);
+    }
+
+
+    public List<Par<F, S>> obtenerTodosLosPares() 
+    {
+        return new ArrayList<>(pares); 
+    }
+
+
+    public void mostrarPares() 
+    {
+        for (Par<F, S> par : pares) 
+        {
+            System.out.println(par);
+        }
+    }
+}
+
+
+import java.util.List;
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Contenedor<String, Integer> contenedor = new Contenedor<>();
+        
+        contenedor.agregarPar("Texto1", 1);
+        contenedor.agregarPar("Texto2", 2);
+        contenedor.agregarPar("Texto3", 3);
+        
+        System.out.println("Todos los pares:");
+        contenedor.mostrarPares();
+        
+        Par<String, Integer> par = contenedor.obtenerPar(1);
+        System.out.println("Par en índice 1: " + par);
+        
+ List<Par<String, Integer>> todosLosPares = contenedor.obtenerTodosLosPares();
+        System.out.println("Lista completa de pares: " + todosLosPares);
+    }
+}
