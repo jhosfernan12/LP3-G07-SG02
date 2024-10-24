5f0d9493ae6c48cc2df04aef7cec86e0d07315f5
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-10-23 21:30:14 -0500

    Create Personaje.java

---
+++ Sesion07/Ejercicios/E1/Personaje.java
@@ -0,0 +1,40 @@
+
+public class Personaje 
+{
+	 private String nombre;
+	    private int vida;
+	    private int ataque;
+	    private int defensa;
+	    private int alcance;
+
+	    public Personaje(String nombre, int vida, int ataque, int defensa, int alcance) 
+	    {
+	        if (vida <= 0 || ataque <= 0 || defensa <= 0 || alcance <= 0) {
+	            throw new IllegalArgumentException("La vida, ataque, defensa y alcance deben ser mayores que 0");
+	        }
+	        this.nombre = nombre;
+	        this.vida = vida;
+	        this.ataque = ataque;
+	        this.defensa = defensa;
+	        this.alcance = alcance;
+	    }
+
+	    public String getNombre() 
+	    {
+	        return nombre;
+	    }
+
+	    @Override
+	    public String toString() 
+	    {
+	        return nombre + " / " + vida + " / " + ataque + " / " + defensa + " / " + alcance;
+	    }
+
+	    public static Personaje fromString(String linea) 
+	    {
+	    	
+	        String[] partes = linea.split(" / ");
+	        return new Personaje(partes[0], Integer.parseInt(partes[1]), Integer.parseInt(partes[2]),
+	                Integer.parseInt(partes[3]), Integer.parseInt(partes[4]));
+	    }
+}
