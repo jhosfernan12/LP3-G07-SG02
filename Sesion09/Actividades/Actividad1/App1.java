fa8999df6d15668def27614717f728c74ce68640
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-11-08 10:07:25 -0500

    Create App1.java

---
+++ Sesion09/Actividades/Actividad1/App1.java
@@ -0,0 +1,14 @@
+package s9;
+import javax.swing.JFrame;
+
+public class App1 
+{
+	public static void main(String[] args)
+	 {
+	 MarcoRastreadorRaton marcoRastreadorRaton = new MarcoRastreadorRaton();
+	 marcoRastreadorRaton.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 marcoRastreadorRaton.setSize(300, 100);
+	 marcoRastreadorRaton.setVisible(true);
+	 }
+
+}
