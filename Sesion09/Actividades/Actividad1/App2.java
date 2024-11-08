3e64484ec8f229214508ead3ec8689fbe34e535f
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-11-08 10:07:39 -0500

    Create App2.java

---
+++ Sesion09/Actividades/Actividad1/App2.java
@@ -0,0 +1,13 @@
+package s9;
+import javax.swing.JFrame;
+
+public class App2 
+{
+	public static void main(String[] args) 
+	{
+		 MarcoDetallesRaton marcoDetallesRaton = new MarcoDetallesRaton();
+		 marcoDetallesRaton.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 marcoDetallesRaton.setSize(400, 150);
+		 marcoDetallesRaton.setVisible(true);
+	}
+}
