c0ac431f2f5d14f911dfd503f8ca120b8e5c9ef8
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-11-08 10:10:36 -0500

    Create MarcoPanel.java

---
+++ Sesion09/Actividades/Actividad2/MarcoPanel.java
@@ -0,0 +1,29 @@
+package s9;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+
+public class MarcoPanel extends JFrame
+{
+	private final JPanel panelBotones; // panel que contiene los botones
+	private final JButton[] botones;
+	 // constructor sin argumentos
+	 public MarcoPanel()
+	 {
+		 super("Demostracion de Panel");
+		 botones = new JButton[5];
+		 panelBotones = new JPanel();
+		 panelBotones.setLayout(new GridLayout(1, botones.length));
+
+		 for (int cuenta = 0; cuenta < botones.length; cuenta++)
+		 {
+			 botones[cuenta] = new JButton("Boton " + (cuenta + 1));
+			 panelBotones.add(botones[cuenta]); 
+		 }
+		 add(panelBotones, BorderLayout.SOUTH);
+	 }
+	 
+}
