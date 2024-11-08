b577f7d4aa630a97ce563fc5817694cb8d246ea9
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-11-08 10:07:54 -0500

    Create App3.java

---
+++ Sesion09/Actividades/Actividad1/App3.java
@@ -0,0 +1,13 @@
+package s9;
+import javax.swing.JFrame;
+
+public class App3 
+{
+	public static void main(String[] args) 
+	{
+		 MarcoDemoTeclas marcoDemoTeclas = new MarcoDemoTeclas();
+		 marcoDemoTeclas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 marcoDemoTeclas.setSize(350, 100);
+		 marcoDemoTeclas.setVisible(true);
+	}
+}
