ee3dbdcef56eec9ad7798c47a517836fd700af81
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-11-24 23:11:59 -0500

    Create MainCommand.java

---
+++ Sesion11/Actividades/MainCommand.java
@@ -0,0 +1,34 @@
+package s11;
+
+public class MainCommand 
+{
+	 public static void main(String[] args) 
+	 {
+	
+	     Televisor televisor = new Televisor();
+	     
+	
+	     Comando encender = new EncenderComando(televisor);
+	     Comando apagar = new ApagarComando(televisor);
+	     Comando subirVolumen = new SubirVolumenComando(televisor);
+	     Comando bajarVolumen = new BajarVolumenComando(televisor);
+	     Comando cambiarCanal = new CambiarCanalComando(televisor);
+	     
+	
+	     ControlRemoto controlRemoto = new ControlRemoto();
+	     
+	    
+	     controlRemoto.establecerComandoEncender(encender);
+	     controlRemoto.establecerComandoApagar(apagar);
+	     controlRemoto.establecerComandoSubirVolumen(subirVolumen);
+	     controlRemoto.establecerComandoBajarVolumen(bajarVolumen);
+	     controlRemoto.establecerComandoCambiarCanal(cambiarCanal);
+	     
+	
+	     controlRemoto.pulsarEncender();
+	     controlRemoto.pulsarSubirVolumen();
+	     controlRemoto.pulsarCambiarCanal();
+	     controlRemoto.pulsarBajarVolumen();
+	     controlRemoto.pulsarApagar();
+	 }
+}
