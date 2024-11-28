1fb73d8316c9cb278d72fe4504be42b48c0a4a22
PachecoJhosFer12 <36451562+jhosfernan12@users.noreply.github.com>
2024-11-28 01:14:52 -0500

    Create Notificacion.java

---
+++ Sesion11/Ejercicios/Ejercicio1/Notificacion.java
@@ -0,0 +1,16 @@
+package s11;
+
+public class Notificacion
+{
+    private String mensaje;
+
+    public Notificacion(String mensaje) 
+    {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() 
+    {
+        return mensaje;
+    }
+}
